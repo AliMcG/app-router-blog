ad8b6884ef4efc55c8d746974d8ac9c90bab8470
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-01-14 20:40:47 +0000

    Update tailwind config and pkg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,18 @@ export default {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        monts: ['var(--font-monts)'],
+        frank: ['var(--font-frank)']
       },
+      color: {
+        customBlue: '#F2F7FF'
+      },
+      backgroundColor: {
+        // Adds the custom background clour to use in the code.
+        custom: {
+          blue: 'var(--color-custom-blue)'
+        }
+      }
     },
   },
   plugins: [],
