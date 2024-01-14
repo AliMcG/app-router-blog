45afd9472429a55e729684e5c39e6580ac9a1963
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-01-14 22:25:21 +0000

    Add edit and delete buttons

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,18 +1,49 @@
-"use client"
+"use client";
 
 import { api } from "~/trpc/react";
 import type { BlogPostPostgres } from "~/types";
+import { useRouter } from "next/navigation";
 import BlogContainer from "@/_components/BlogContainer";
-
+import { useSession } from "next-auth/react";
 
 export default function BlogPage({ params }: { params: { id: string } }) {
+  const router = useRouter();
+  const { data } = api.post.locate.useQuery({ id: params.id });
+  const { mutate } = api.post.delete.useMutation();
+  const { data: session } = useSession();
 
-  const { data } = api.post.locate.useQuery({ id: params.id })
- 
+  const DeleteButton = () => {
+    return (
+      <button
+        className="text-red-500"
+        // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+        onClick={() => mutate({ id: data?.id as string })}
+      >
+        Delete
+      </button>
+    );
+  };
+  const EditButton = () => {
+    return (
+      <button
+        className="text-blue-500"
+        // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+        onClick={() => void router.push(`/add/${data?.id as string}`)}
+      >
+        Edit
+      </button>
+    );
+  };
 
   return (
     <div className="m-auto flex flex-col items-center">
-        <BlogContainer data={data as BlogPostPostgres}/>
+      <BlogContainer data={data as BlogPostPostgres} />
+      {session?.user.role === "ADMIN" && (
+        <>
+          <DeleteButton />
+          <EditButton />
+        </>
+      )}
     </div>
   );
 }
