7a79bfbe5efd4adfecc5472ac8a20811fee88a11
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-01-14 22:25:06 +0000

    Add all routes

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -7,49 +7,119 @@ import {
 } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-    list: publicProcedure.query(async ({ ctx }) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-      const result = await ctx.db.blog.findMany()
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return result
-    }),
-    locate: publicProcedure.input(z.object({ id: z.string()})).query(async ({ ctx, input }) => {
+  list: publicProcedure.query(async ({ ctx }) => {
+    const result = await ctx.db.blog.findMany();
+    return result;
+  }),
+  locate: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
       const result = await ctx.db.blog.findUnique({
         where: {
           id: input.id,
         },
+      });
+      return result;
+    }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        title: z.string().min(1),
+        description: z.string(),
+        image: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const result = await ctx.db.blog.create({
+        data: {
+          title: input.title,
+          description: input.description,
+          image: input.image,
+          numberVotes: 0,
+          createdAt: new Date(),
+        },
+      });
+      return result;
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        title: z.string(),
+        description: z.string(),
+        image: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const updatedBlog = await ctx.db.blog.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          title: input.title,
+          description: input.description,
+          image: input.image,
+          createdAt: new Date(),
+        },
+      });
+      return updatedBlog;
+    }),
+  listBySerachText: protectedProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const result = await ctx.db.blog.findMany({
+        where: {
+          OR: [
+            {
+              title: {
+                contains: input.text,
+              },
+            },
+            {
+              description: {
+                contains: input.text,
+              },
+            },
+          ],
+        },
+      });
+      return result;
+    }),
+  delete: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const result = await ctx.db.blog.delete({
+        where: {
+          id: input.id,
+        },
+      });
+      return result;
+    }),
+    updateBlogVoteCount: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
       })
-      return result
-    })
-  // create: protectedProcedure
-  //   .input(z.object({ name: z.string().min(1) }))
-  //   .mutation(async ({ ctx, input }) => {
-  //     // simulate a slow db call
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  //     return ctx.db.post.create({
-  //       data: {
-  //         name: input.name,
-  //         createdBy: { connect: { id: ctx.session.user.id } },
-  //       },
-  //     });
-  //   }),
-
-  // getLatest: protectedProcedure.query(({ ctx }) => {
-  //   return ctx.db.post.findFirst({
-  //     orderBy: { createdAt: "desc" },
-  //     where: { createdBy: { id: ctx.session.user.id } },
-  //   });
-  // }),
-
-  // getSecretMessage: protectedProcedure.query(() => {
-  //   return "you can now see this secret message!";
-  // }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const result = await ctx.db.blog.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          numberVotes: {
+            increment: 1,
+          },
+        },
+      });
+      return result;
+    }),
 });
