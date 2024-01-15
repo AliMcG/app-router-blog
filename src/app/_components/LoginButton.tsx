bcc79792e1da31f4ddbf7d19425b7b466b821b98
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-01-15 21:41:44 +0000

    Add tooltip to log in button

--- src/app/_components/LoginButton.tsx
+++ src/app/_components/LoginButton.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import { useSession } from "next-auth/react";
+import Tooltip from "@mui/material/Tooltip";
 
 const LoginButton = () => {
   const { data: session } = useSession();
   return (
-    <Link
-      href={session ? "/api/auth/signout" : "/api/auth/signin"}
-      className="bg-blue/10 text-blue hover:bg-blue/60 w-28 rounded border-2 border-[#CFE1FF] px-3 py-1 font-semibold text-gray-700 no-underline transition"
-    >
-      {session ? "Sign out" : "Sign in"}
-    </Link>
+    <Tooltip title="Sign in to Heart a blog">
+      <Link
+        href={session ? "/api/auth/signout" : "/api/auth/signin"}
+        className="bg-blue/10 text-blue hover:bg-blue/60 w-28 rounded border-2 border-[#CFE1FF] px-3 py-1 font-semibold text-gray-700 no-underline transition"
+      >
+        {session ? "Sign out" : "Sign in"}
+      </Link>
+    </Tooltip>
   );
 };
 
