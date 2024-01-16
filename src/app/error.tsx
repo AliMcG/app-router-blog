be3bfadfc544863a3a9732acaf7bd4de2bd44726
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-01-16 21:48:51 +0000

    Update router for public search and style to error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,30 +1,35 @@
-'use client' // Error components must be Client Components
- 
-import { useEffect } from 'react'
- 
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
 export default function Error({
   error,
   reset,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="flex justify-center p-4">
+      <div className="my-10 flex flex-col items-center border-2 border-[#CFE1FF]">
+        <h2 className="mb-4 p-10 font-monts text-2xl font-semibold text-[#073D93] hover:text-[#067A75]">
+          Something went wrong!
+        </h2>
+        <h3 className="mb-4 font-monts text-xl font-semibold text-[#073D93] hover:text-[#067A75]">
+          Try refreshing the page
+        </h3>
+        <button
+          className='bg-blue/10 text-blue hover:bg-blue/60 transition" mb-10 w-28 rounded border-2 border-[#CFE1FF] px-3 py-1 font-semibold text-gray-700 no-underline'
+          onClick={() => reset()}
+        >
+          Refresh
+        </button>
+      </div>
     </div>
-  )
+  );
 }
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -64,7 +64,7 @@ export const postRouter = createTRPCRouter({
       });
       return updatedBlog;
     }),
-  listBySerachText: protectedProcedure
+  listBySerachText: publicProcedure
     .input(
       z.object({
         text: z.string(),
