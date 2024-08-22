0f0d879485455a3801baeb9ca483c675f5205ac6
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-08-22 21:27:14 +0100

    Turn off non-nullable-type-assertion rule for typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/non-nullable-type-assertion-style": "off",
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
